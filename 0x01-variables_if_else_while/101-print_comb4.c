6cb6803fbb148f524715c6a274350fe48add12b0
wanjikukariuki <charitykariuki99@yahoo.com>
2023-04-24 11:04:12 -0700

    more of what you do

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n, m, i;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (i = 50; i < 58; i++)
+			{
+				if (i > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(i);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

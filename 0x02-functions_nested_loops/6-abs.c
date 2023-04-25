3c4f84d98de3613d1e0605a3e0b89483e9c547af
wanjikukariuki <charitykariuki99@yahoo.com>
2023-04-25 12:38:52 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#incude "main.h"
+/**
+ * _abs - prints the absolute value of an integer
+ * @a: the interger
+ * Return: returns the abs value
+ */
+int _abs(int a)
+{
+	int i = a;
+	int absvalue;
+
+	if (i < 0)
+	{
+		i = i * (-1);
+	}
+	absvalue = i
+		return (absvalue);
+}

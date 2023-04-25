3e56f41d07306cc46963a8309693e17b9b330f88
wanjikukariuki <charitykariuki99@yahoo.com>
2023-04-25 15:03:11 -0700

    computer class

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - prints the first 50 fibonacci numbers
- * Return: Always 0
+ *main - Prints the add of the Fibonacci numbers
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -10,14 +11,15 @@ int main(void)
 
 	n1 = 1;
 	n2 = 2;
-	printf("%d, %d, n1, n2);
+	printf("%ld, %ld", n1, n2);
 	for (c = 0; c < 48; c++)
 	{
 		fn = n1 + n2;
-		printf(",%d", fn);
+		printf(", %ld", fn);
 		n1 = n2;
 		n2 = fn;
 	}
 	printf("\n");
 	return (0);
 }
+

2c813c874c6adfb716c789cf5887609ca3679552
wanjikukariuki <charitykariuki99@yahoo.com>
2023-04-25 11:24:47 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print alphabet in lower case
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	void print_alphabet(void);
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+		_putchar(k);
+	_putchar('\n');
+}

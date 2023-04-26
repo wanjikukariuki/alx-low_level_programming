86068576715d3d69cbc55532af09418818002f51
wanjikukariuki <charitykariuki99@yahoo.com>
2023-04-26 08:52:29 +0300

    fear computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: input string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}

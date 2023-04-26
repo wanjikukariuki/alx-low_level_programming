7e93a451dd1fae58e125933ff43ad6aa09b59716
wanjikukariuki <charitykariuki99@yahoo.com>
2023-04-26 10:25:15 -0700

    math table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,39 +8,37 @@
  */
 void print_times_table(int n)
 {
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
 	int a, b, op;
 
-	if (n >= 0 && n <= 15)
-	{
 		for (a = 0; a <= n; a++)
 		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
+			for (b = 0; b <= n; b++)
 			{
 				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
+
+				if (op > 99)
 				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
+					continue;
 				}
-				else if (op <= 99)
+				if (b > 0)
 				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(' ');
 				}
-				else
+				if (op < 10)
 				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(' ');
+				}
+				_putchar((op / 10) + '0');
+				_putchar((op % 10) + '0');
+				if (b < n)
+				{
+					_putchar(',');
 				}
 			}
 			_putchar('\n');
 		}
-	}
 }
-

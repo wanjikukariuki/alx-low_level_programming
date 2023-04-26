debc3fedb31a99c667c541c7a77d49882384d70f
wanjikukariuki <charitykariuki99@yahoo.com>
2023-04-26 21:22:59 +0300

    paranoid in reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,10 @@
-#include main.h"
+#include "main.h"
 /**
- * reverse_array - reverses the content of an array of integers
- * @a: an array of integers
- * @n: the number of elements to swap
- * Return : void
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ *
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {

aa5c23b1d8ab977e762572e942dce55276c98ac4
wanjikukariuki <charitykariuki99@yahoo.com>
2023-04-27 09:12:55 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ * @size: size of the triangle
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 
+	}
+}

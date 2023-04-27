1f65fde5bea556fb5fce550a4159c5b3cf6802fc
wanjikukariuki <charitykariuki99@yahoo.com>
2023-04-27 07:23:02 -0700

    numbers speak

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers 0 - 9
+ * return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; 1 < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	{
+		_putchar('\n');
+	}
+}

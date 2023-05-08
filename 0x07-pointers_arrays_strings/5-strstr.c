8978a17151fde676f5172ea86bc822c492a8c3d2
wanjikukariuki <charitykariuki99@yahoo.com>
2023-05-09 01:35:37 +0300

    haystack

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,30 +7,18 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	if (*needle == '\0')
-	{
-		return (haystack);
-	}
-	char *p = haystack;
+	 for (; *haystack != '\0'; haystack++)
+	 {
+		  char *l = haystack;
+		   char *p = needle;
 
-	while (*p != '\0')
-	{
-		if (*p == *needle)
-		{
-			char *q = p + 1;
-			char *r = needle + 1;
-
-			while (*r != '\0' && *q == *r)
-			{
-				q++;
-				r++;
-			}
-			if (*r == '\0')
-			{
-				return (p);
-			}
-		}
-		p++;
-	}
-	return (NULL);
+		   while (*l == *p && *p != '\0')
+		   {
+			   l++;
+			   p++;
+		   }
+		   if (*p == '\0')
+			   return (haystack);
+	 }
+	 return (0);
 }

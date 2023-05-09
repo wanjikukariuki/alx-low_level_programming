2c44ffffb5575eb3a82706db486af2e5d5beb4f6
wanjikukariuki <charitykariuki99@yahoo.com>
2023-05-09 19:37:03 +0300

    task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programmig is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+	for (; j < i; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[j] = '\0';
+	return (dest);
+}

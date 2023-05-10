d2ecff6c674e4747fa80f95cdf7a8398c0620a57
wanjikukariuki <charitykariuki99@yahoo.com>
2023-05-10 15:52:47 +0300

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * return: 0 
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}

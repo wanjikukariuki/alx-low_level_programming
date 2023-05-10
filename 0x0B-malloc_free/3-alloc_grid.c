b1f93cd7c7af03d1c0edfa9e84144029adad94a5
wanjikukariuki <charitykariuki99@yahoo.com>
2023-05-10 12:21:06 +0300

    dream

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 		if (array[i] == NULL)
 		{
 			free(array);
-			for (j= 0; j <= i; j++)
+			for (j = 0; j <= i; j++)
 				free(array[j]);
 			return (NULL);
 		}

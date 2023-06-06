7b24c11b085d7d90494ea6ef781e630afd8219c5
wanjikukariuki <charitykariuki99@yahoo.com>
2023-06-06 14:09:06 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,11 +14,16 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] == '0')
+			bit_value = (bit_value << 1);
+
+		else if (b[i] == '1')
+			bit_value = (bit_value << 1) | 1;
+
+		else
 			return (0);
-		bit_value = 2 * value + (b[i] - '0');
 	}
 	return (bit_value);
-
+}

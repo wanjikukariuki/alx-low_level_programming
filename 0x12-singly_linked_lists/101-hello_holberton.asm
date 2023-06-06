3b22b2f5546c6d4fe24703614d6766ca9c370d8c
wanjikukariuki <charitykariuki99@yahoo.com>
2023-06-06 04:23:20 +0300

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
-section .data
-    message db "Hello, Holberton\n", 0
-    format db "%s", 10, 0
-
-section .text
-    extern printf
-
-global _start
-_start:
-    ; Prepare arguments for printf
-    mov rdi, format
-    mov rsi, message
-    xor rax, rax  ; Clear RAX register for variadic functions
-    call printf
-
-    ; Exit the program
-    mov eax, 60  ; System call number for exit
-    xor edi, edi ; Exit status 0
-    syscall
+global   main
+	  extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
